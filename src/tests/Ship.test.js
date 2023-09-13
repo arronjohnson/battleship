c0973a14d8e0f23b4a6d331dc670a57b8d08f3f7
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-09-13 11:31:28 +0100

    Add Ship unit tests

---
+++ src/tests/Ship.test.js
@@ -0,0 +1,23 @@
+import Ship from '../modules/Ship';
+
+let ship;
+
+beforeEach(() => (ship = new Ship(5)));
+
+test('length', () => expect(ship.length).toBe(5));
+
+describe('methods', () => {
+  test('hit', () => {
+    for (let i = 0; i < 3; i++) {
+      ship.hit();
+    }
+    expect(ship.hitCount).toBe(3);
+  });
+
+  test('isSunk', () => {
+    for (let i = 0; i < 5; i++) {
+      ship.hit();
+    }
+    expect(ship.isSunk()).toBe(true);
+  });
+});
