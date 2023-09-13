e9b3ad92ea8459ca4a02798e64933c9352ef4e55
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-09-13 20:39:09 +0100

    Add Game class
    Preliminary class to handle the main game loop.
    
    Currently allows a single human player vs. an AI opponent. All ships
    are placed randomly at the start of a game.
    
    isGameOver() and getWinner() are used to announce a winner once either
    player's ships have all been sunk.

---
+++ src/modules/Game.js
@@ -0,0 +1,26 @@
+import Player from './Player';
+
+export default class Game {
+  constructor() {
+    this.resetGame();
+    this.startGame();
+  }
+
+  resetGame() {
+    this.p1 = new Player('player');
+    this.p2 = new Player('computer');
+  }
+
+  startGame() {
+    this.p1.gameboard.placeShipsRandomly();
+    this.p2.gameboard.placeShipsRandomly();
+  }
+
+  isGameOver() {
+    return this.p1.gameboard.allShipsSunk() || this.p2.gameboard.allShipsSunk();
+  }
+
+  getWinner() {
+    return this.p2.gameboard.allShipsSunk ? 'player' : 'computer';
+  }
+}
