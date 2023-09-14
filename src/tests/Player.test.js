12fc06efb5811a430c73510fe8b727bf8b0c2370
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-09-14 13:03:44 +0100

    Remove redundant Player tests

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,42 +1,13 @@
+import Gameboard from '../modules/Gameboard';
 import Player from '../modules/Player';
 
 jest.mock('../modules/Gameboard');
 
-let computer;
-let player;
+let p1;
 
-beforeEach(() => {
-  computer = new Player('computer');
-  player = new Player('player');
-});
+beforeEach(() => (p1 = new Player('player')));
 
-test('name', () => expect(player.name).toBe('player'));
-
-describe('turns', () => {
-  beforeEach(() => player.startTurn());
-
-  test('startTurn', () => expect(player.isTurn).toBe(true));
-
-  test('endTurn', () => {
-    player.endTurn();
-    expect(player.isTurn).toBe(false);
-  });
-});
-
-describe('attacking', () => {
-  beforeEach(() => player.startTurn());
-
-  test("not player's turn", () => {
-    expect(computer.attack(player, [0, 0])).toBe(false);
-  });
-
-  test('receiveAttack called', () => {
-    player.attack(computer, [0, 0]);
-    expect(computer.gameboard.receiveAttack).toHaveBeenCalledWith([0, 0]);
-  });
-
-  test('receiveRandomAttack called', () => {
-    player.attackRandom(computer);
-    expect(computer.gameboard.receiveRandomAttack).toHaveBeenCalled();
-  });
+test('create new player', () => {
+  expect(p1.name).toBe('player');
+  expect(Gameboard).toHaveBeenCalled();
 });
