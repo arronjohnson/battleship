345e4f5867cd77ed9df31e7b27611ad23cc25a1f
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-09-14 14:49:19 +0100

    Update Game methods
    Removed resetGame and startGame, as these were unnecessary. The UI
    creates a new instance of Game each time the game restarts.
    
    Moved player creation and ship placement to the constructor.

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -2,16 +2,8 @@ import Player from './Player';
 
 export default class Game {
   constructor() {
-    this.resetGame();
-    this.startGame();
-  }
-
-  resetGame() {
     this.p1 = new Player('player');
     this.p2 = new Player('computer');
-  }
-
-  startGame() {
     this.p1.gameboard.placeShipsRandomly();
     this.p2.gameboard.placeShipsRandomly();
   }
@@ -22,5 +14,13 @@ export default class Game {
 
   getWinner() {
     return this.p2.gameboard.allShipsSunk() ? 'player' : 'computer';
+  }
+
+  playerTurn(x, y) {
+    return this.p2.gameboard.receiveAttack([x, y]);
+  }
+
+  computerTurn() {
+    return this.p1.gameboard.receiveRandomAttack();
   }
 }
